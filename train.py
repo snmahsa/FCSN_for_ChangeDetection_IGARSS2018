b84cc22d3da55c4a4ce883a09f78a43c22564325
mahsa <mahsasanaei.n@gmail.com>
2024-09-14 12:31:01 +0330

    Edit Teain.py

--- train.py
+++ train.py
@@ -6,7 +6,7 @@ from torch import nn
 from torch.optim import SGD
 from torch.utils.data import Dataset, DataLoader
 from model import FC_EF
-
+form module import display_images
 class OSCD(Dataset):
     def __init__(self, dir_nm):
         super(OSCD, self).__init__()
@@ -28,6 +28,9 @@ def main():
     train_dir = './data/train/'
     test_dir = './data/test/'
     lr = 0.001
+    test_all_preds = []
+    test_all_labels = []
+
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     train_data = OSCD(train_dir)
@@ -36,10 +39,14 @@ def main():
     test_dataloader = DataLoader(test_data, batch_size=1, shuffle=True)
     model = FC_EF().to(device, dtype=torch.float)
 
-    optimizer = SGD(model.parameters(), lr=lr, momentum=0.9, weight_decay=0.0005)
-    criterion = nn.CrossEntropyLoss()
+    # optimizer = SGD(model.parameters(), lr=lr, momentum=0.9, weight_decay=0.0005)
+    optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+    # criterion = nn.CrossEntropyLoss()
+    weights = torch.tensor([1.0, 20.0]).to(device)
+    criterion = nn.CrossEntropyLoss(weight =weights)
 
-    for epoch in range(10):
+    for epoch in range(70):
+        print(f"-----------------------epoch {epoch} :-----------------------")
         loss_v = []
         model.train()
         for i, data in enumerate(train_dataloader):
@@ -48,31 +55,58 @@ def main():
             x2 = x2.to(device, dtype=torch.float)
             lbl = lbl.to(device, dtype=torch.long)
             y = model(x1,x2)
+            predicted_labels = torch.argmax(y, dim=1)
+            if i < 2:
+              print("Dimensions of x1:", x1.shape)
+              print("Dimensions of x2:", x2.shape)
+              print("Dimensions of lbl:", lbl.shape)
+              print("Dimensions of y:", y.shape)
+              print(f"sample {i} :")
+              display_images(x1[0], x2[0], lbl[0], predicted_labels[0])
             optimizer.zero_grad()
             loss = criterion(y, lbl)
             loss.backward()
             optimizer.step()
             loss_v.append(loss.item())
             if(i%20==0 and i>0):
-                print(np.mean(loss_v))
+                print(f"---loss in {i/20} :",np.mean(loss_v))
                 loss_v = []
 
-        loss_v = []
-        model.eval()
+    loss_v = []
+    model.eval()
+    test_all_preds.clear()
+    test_all_labels.clear()
+
+    with torch.no_grad(): 
         for i, data in enumerate(test_dataloader):
             x1, x2, lbl = data
             x1 = x1.to(device, dtype=torch.float)
             x2 = x2.to(device, dtype=torch.float)
             lbl = lbl.to(device, dtype=torch.long)
-            y = model(x1, x2)
-            optimizer.zero_grad()
-            loss = criterion(y, lbl)
-            loss.backward()
-            optimizer.step()
-            loss_v.append(loss.item())
-        print('test:', np.mean(loss_v))
-        loss_v = []
 
+
+            y = model(x1, x2)
+
+            predicted_labels_test = torch.argmax(y, dim=1)
+            predicted_labels_np = predicted_labels_test.cpu().numpy().flatten()
+            lbl_np = lbl.cpu().numpy().flatten()
+
+             
+            test_all_labels.extend(lbl_np)
+            test_all_preds.extend(predicted_labels_np)
+
+            if i < 2:
+                print(f"----test {i} :----")
+                display_images(x1[0], x2[0], lbl[0], predicted_labels_test[0])
+
+        precision = precision_score(test_all_labels, test_all_preds, average='macro')
+        recall = recall_score(test_all_labels, test_all_preds, average='macro')
+        f1 = f1_score(test_all_labels, test_all_preds, average='macro')
+
+        print(f"Precision: {precision}, Recall: {recall}, F1 Score: {f1}")
+
+        print("-------------------------------")
+    torch.save(model.state_dict(), f'model_weights_epoch_{epoch}.pth')
 
 if __name__ == '__main__':
     main()
